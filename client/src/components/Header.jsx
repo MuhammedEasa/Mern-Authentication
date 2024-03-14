3f8095473247732c7d2d2636e3c3bee1e3c7cfc9
Muhammed Easa <easanedumangad@gmail.com>
2024-03-14 17:10:25 +0530

    Created A Good Looking SignUp Page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
-    <div className="bg-slate-200">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+    <div className="bg-gray-50 dark:bg-gray-900 text-zinc-100">
+      <div className="flex justify-between items-center max-w-7xl mx-auto p-3">
         <Link to={"/"}>
           <h1 className="font-bold">Auth App</h1>
         </Link>
 
-        <ul className="flex gap-4">
+        <ul className="flex gap-3">
           <Link to={"/"}>
             <li>Home</li>
           </Link>
