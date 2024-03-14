0c5fb979db1fe122309a7e29ae50a17d6924004e
Muhammed Easa <easanedumangad@gmail.com>
2024-03-14 15:56:14 +0530

    Bcrypt The Password In SignUp And Write the code Of SignUp Controller

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+
 dotenv.config();
 
 const app = express();
@@ -14,8 +16,10 @@ mongoose
     console.error(err);
   });
 
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
